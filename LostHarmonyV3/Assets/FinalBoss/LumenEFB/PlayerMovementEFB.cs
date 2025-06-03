a7e6c2eb2e21b112ebc9ac9a6fbb67623428d95f
Joseph Augusto Flores <josephaugustoflores@gmail.com>
2025-06-03 15:55:53 -0500

    Personalización
    LOLASO

--- LostHarmonyV3/Assets/FinalBoss/LumenEFB/PlayerMovementEFB.cs
+++ LostHarmonyV3/Assets/FinalBoss/LumenEFB/PlayerMovementEFB.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovementEFB : MonoBehaviour
 {
     public float velocidadMovimiento = 5f;
     public float fuerzaSalto = 12f;
